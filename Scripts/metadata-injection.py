ef3e8c21dbaa0502e04bebdc974c05bcb8338d0d
Frederico Martins <fredericomartins@outlook.com>
2017-10-27 14:54:07 +0100

    Usage of Gracenote API for metadata injection

--- Scripts/metadata-injection.py
+++ Scripts/metadata-injection.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
-from mutagen.mp3 import MP3
-from mutagen.id3 import ID3NoHeaderError
-from mutagen.id3 import ID3, TIT2, TALB, TPE1, TPE2, COMM, USLT, TCOM, TCON, TDRC
+#from io import StringIO
+#from mutagen.mp3 import MP3
+from mutagen import id3
 from os import listdir, path
-from sys import  argv
+#from PIL import Image
+from pygn import register, search # https://github.com/cweichen/pygn
+from sys import argv
 from textwrap import dedent
+from urllib import request
 
+
+clientID = '702337754-900975F20663BD0B36A073B2E463DE14'
+userID = register(clientID)
 
 def Parse():
 
@@ -43,13 +49,30 @@ for artist in sorted(listdir(args.source)):
             music_path = path.join(album_path, music)
             
             try: # create ID3 tag if not present
-                tags = ID3(music_path)
-            except ID3NoHeaderError:
+                tags = id3.ID3(music_path)
+            except id3.ID3NoHeaderError:
                 print("Adding ID3 header;")
-                tags = ID3()
+                tags = id3.ID3()
 
-            tags["TIT2"] = TIT2(encoding=3, text=u'{0}'.format(music))
-            tags["TALB"] = TALB(encoding=3, text=u'{0}'.format(album))
-            tags["TPE1"] = TPE1(encoding=3, text=u'{0}'.format(artist))
+            result = search(clientID=clientID, userID=userID, artist=artist, track=music)
+            album_art = request.urlopen(result['album_art_url']).read()
+            # Image.open(StringIO(opened_url)) and Image.show()
+
+            for each in result['genre'].keys():
+                if result['genre'][each]['TEXT'] in genres:
+                    result['genre'] = result['genre'][each]['TEXT']
+                    break
+            else:
+                print("Failed with no appropriate genre found in:", result['genre'])
+                exit(1)
+
+            tags["TIT2"] = id3.TIT2(encoding=3, text=u'{0}'.format(music))
+            tags["TALB"] = id3.TALB(encoding=3, text=u'{0}'.format(result['album_title']))
+            tags["TPE1"] = id3.TPE1(encoding=3, text=u'{0}'.format(artist))
+            tags["TCON"] = id3.TCON(encoding=3, text=u'{0}'.format(result['genre']))
+            tags["TDRC"] = id3.TDRC(encoding=3, text=u'{0}'.format(result['album_year']))
+            tags["TRCK"] = id3.TRCK(encoding=3, text=u'{0}'.format(result['track_number']))
+            tags["APIC"] = id3.APIC(encoding=3, mime='image/png' if cover.endswith('png') else 'image/jpeg',
+                type=3, desc=u'Cover', data=album_art)
 
             tags.save(music_path)

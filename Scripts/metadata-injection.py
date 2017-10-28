51eea2055054c04e4221aeaaf52bd51a8e95ddb3
Frederico Martins <fredericomartins@outlook.com>
2017-10-28 03:13:26 +0100

    Metadata injection features add and bug resolution

--- Scripts/metadata-injection.py
+++ Scripts/metadata-injection.py
@@ -12,14 +12,14 @@ from urllib import request
 clientID = '702337754-900975F20663BD0B36A073B2E463DE14'
 userID = register(clientID)
 genres = ['Indie Rock', 'Acid Rock', 'Trance', 'Reggae', 'Grunge', 'New Romantic', 'Hard Rock', 'Southern Rap', 'Emo & Hardcore', 'Pop Punk', 'Rap Comedy',
-    'Heavy Metal', 'Rock']
+    'Heavy Metal', 'Rock', 'Classical', 'Brazilian Pop', 'European Traditional', 'Samba', 'Industrial', 'Other', 'Synth Pop', 'Alternative Rock']
 
 def Parse():
 
     parser = ArgumentParser(prog=path.basename(__file__.rpartition('.')[0]), add_help=False, formatter_class=RawDescriptionHelpFormatter,
                 description=dedent('''\
                               Metadata
-                        -------------------
+                        ------------------
                         Metadata Injection
                     '''), epilog=dedent('''\
                         Check the git repository at https://github.com/flippym/spytify-server,
@@ -28,6 +28,7 @@ def Parse():
                 )
 
     optional = parser.add_argument_group('Flags')
+    optional.add_argument('-b', '--begin', metavar='band', type=str, help='Start injecting from band given')
     optional.add_argument('-s', '--source', metavar='path', type=str, help='Source path with music', required=True)
     optional.add_argument('-h', '--help', action='help', help='Show this help message')
     
@@ -46,13 +47,20 @@ for artist in sorted(listdir(args.source)):
         album_path = path.join(artist_path, album)
         for music in listdir(album_path):         
             music_path = path.join(album_path, music)
-            
+            print(artist, music)
             try: # create ID3 tag if not present
                 tags = id3.ID3(music_path)
             except id3.ID3NoHeaderError:
                 tags = id3.ID3()
 
-            result = search(clientID=clientID, userID=userID, artist=artist, track=music)
+            try:
+                result = search(clientID=clientID, userID=userID, artist=artist, track=music)
+                if not result['album_art_url']:
+                    raise UnboundLocalError
+            except UnboundLocalError:
+                print("Failed query with artist '{0}' and music '{1}'".format(artist, music))
+                continue
+
             album_art = request.urlopen(result['album_art_url']).read()
 
             for each in result['genre'].keys():
@@ -63,19 +71,22 @@ for artist in sorted(listdir(args.source)):
                 print("Failed with no appropriate genre found for '{0} - {1}':\n{2}".format(artist, music, result['genre']))
                 continue
 
-            tags["TIT2"] = id3.TIT2(encoding=3, text=u'{0}'.format(music))
-            tags["TALB"] = id3.TALB(encoding=3, text=u'{0}'.format(result['album_title']))
-            tags["TPE1"] = id3.TPE1(encoding=3, text=u'{0}'.format(artist))
-            tags["TCON"] = id3.TCON(encoding=3, text=u'{0}'.format(result['genre']))
-            tags["TDRC"] = id3.TDRC(encoding=3, text=u'{0}'.format(result['album_year']))
-            tags["TRCK"] = id3.TRCK(encoding=3, text=u'{0}'.format(result['track_number']))
-            tags["APIC"] = id3.APIC(encoding=3, mime='image/png' if '.png' in result['album_art_url'] else 'image/jpeg',
+            if tags['TALB'] and tags['TALB'] is not 'Unknown Album':
+                result['album_title'] = str(tags['TALB'])
+
+            tags['TIT2'] = id3.TIT2(encoding=3, text=u'{0}'.format(music))
+            tags['TALB'] = id3.TALB(encoding=3, text=u'{0}'.format(result['album_title']))
+            tags['TPE1'] = id3.TPE1(encoding=3, text=u'{0}'.format(artist))
+            tags['TCON'] = id3.TCON(encoding=3, text=u'{0}'.format(result['genre']))
+            tags['TDRC'] = id3.TDRC(encoding=3, text=u'{0}'.format(result['album_year']))
+            tags['TRCK'] = id3.TRCK(encoding=3, text=u'{0}'.format(result['track_number']))
+            tags['APIC'] = id3.APIC(encoding=3, mime='image/png' if '.png' in result['album_art_url'] else 'image/jpeg',
                 type=3, desc=u'Cover', data=album_art)
 
             tags.save(music_path)
 
             if album is not result['album_title']:
-                real_album_path = path.join(artist_path, result['album_title'])
+                real_album_path = path.join(artist_path, ''.join(result['album_title'].split('/')))
                 try:
                     mkdir(real_album_path)
                 except FileExistsError:

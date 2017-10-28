fa4c7afc91fc8b79091259cb2d717add5e67495d
Frederico Martins <fredericomartins@outlook.com>
2017-10-28 12:54:55 +0100

    Metadata injection filter possibility added and bug resolution

--- Scripts/metadata-injection.py
+++ Scripts/metadata-injection.py
@@ -6,13 +6,18 @@ from os import listdir, mkdir, path, rename, rmdir
 from pygn import register, search # https://github.com/cweichen/pygn
 from sys import argv
 from textwrap import dedent
-from urllib import request
+from urllib import error, request
 
 
 clientID = '702337754-900975F20663BD0B36A073B2E463DE14'
 userID = register(clientID)
+relate = {'ACDC': 'AC/DC'}
 genres = ['Indie Rock', 'Acid Rock', 'Trance', 'Reggae', 'Grunge', 'New Romantic', 'Hard Rock', 'Southern Rap', 'Emo & Hardcore', 'Pop Punk', 'Rap Comedy',
-    'Heavy Metal', 'Rock', 'Classical', 'Brazilian Pop', 'European Traditional', 'Samba', 'Industrial', 'Other', 'Synth Pop', 'Alternative Rock']
+    'Heavy Metal', 'Rock', 'Classical', 'Brazilian Pop', 'European Traditional', 'Samba', 'Industrial', 'Other', 'Synth Pop', 'Alternative Rock', 'House',
+    'Electronica', 'Electric Blues', 'Punk', 'Urban', 'Old School Punk', 'Western Pop', 'Classic R&B', 'Classic Soul', 'Jazz', 'New Wave Rock', 'Brit Pop', 
+    'Folk', 'Pop Electronica', 'Contemporary R&B', 'European Pop', 'Progressive House', 'Western Pop', 'New Wave Pop', 'East Coast Rap', 'Gangsta Rap',
+    'West Coast', 'Garage Rock Revival', 'Funk', 'Alternative', 'Midwestern Rap', 'Funk Metal', 'Lounge', 'Brit Rock', 'Classic Country', 'Caribbean Pop',
+    'African Pop']
 
 def Parse():
 
@@ -28,7 +33,7 @@ def Parse():
                 )
 
     optional = parser.add_argument_group('Flags')
-    optional.add_argument('-b', '--begin', metavar='band', type=str, help='Start injecting from band given')
+    optional.add_argument('-f', '--filter', metavar='exp', type=str, help='Artist comma delimited expression')
     optional.add_argument('-s', '--source', metavar='path', type=str, help='Source path with music', required=True)
     optional.add_argument('-h', '--help', action='help', help='Show this help message')
     
@@ -43,25 +48,31 @@ args = Parse()
 
 for artist in sorted(listdir(args.source)):
     artist_path = path.join(args.source, artist)
+    if args.filter and artist not in args.filter.split(','):
+        continue
     for album in listdir(artist_path):
         album_path = path.join(artist_path, album)
         for music in listdir(album_path):         
             music_path = path.join(album_path, music)
-            print(artist, music)
             try: # create ID3 tag if not present
                 tags = id3.ID3(music_path)
             except id3.ID3NoHeaderError:
                 tags = id3.ID3()
 
             try:
-                result = search(clientID=clientID, userID=userID, artist=artist, track=music)
-                if not result['album_art_url']:
-                    raise UnboundLocalError
+                if artist in relate:
+                    real_artist = relate[artist]
+                else:
+                    real_artist = artist
+                result = search(clientID=clientID, userID=userID, artist=real_artist, track=music)
             except UnboundLocalError:
                 print("Failed query with artist '{0}' and music '{1}'".format(artist, music))
                 continue
 
-            album_art = request.urlopen(result['album_art_url']).read()
+            try:
+                album_art = request.urlopen(result['album_art_url']).read()
+            except error.URLError:
+                pass
 
             for each in result['genre'].keys():
                 if result['genre'][each]['TEXT'] in genres:
@@ -71,17 +82,18 @@ for artist in sorted(listdir(args.source)):
                 print("Failed with no appropriate genre found for '{0} - {1}':\n{2}".format(artist, music, result['genre']))
                 continue
 
-            if tags['TALB'] and tags['TALB'] is not 'Unknown Album':
+            if str(tags['TALB']) and str(tags['TALB']) != 'Unknown Album':
                 result['album_title'] = str(tags['TALB'])
 
             tags['TIT2'] = id3.TIT2(encoding=3, text=u'{0}'.format(music))
             tags['TALB'] = id3.TALB(encoding=3, text=u'{0}'.format(result['album_title']))
-            tags['TPE1'] = id3.TPE1(encoding=3, text=u'{0}'.format(artist))
+            tags['TPE1'] = id3.TPE1(encoding=3, text=u'{0}'.format(result['album_artist_name']))
             tags['TCON'] = id3.TCON(encoding=3, text=u'{0}'.format(result['genre']))
             tags['TDRC'] = id3.TDRC(encoding=3, text=u'{0}'.format(result['album_year']))
             tags['TRCK'] = id3.TRCK(encoding=3, text=u'{0}'.format(result['track_number']))
-            tags['APIC'] = id3.APIC(encoding=3, mime='image/png' if '.png' in result['album_art_url'] else 'image/jpeg',
-                type=3, desc=u'Cover', data=album_art)
+            if result['album_art_url']:
+                    tags['APIC'] = id3.APIC(encoding=3, mime='image/png' if '.png' in result['album_art_url'] else 'image/jpeg',
+                        type=3, desc=u'Cover', data=album_art)
 
             tags.save(music_path)
 

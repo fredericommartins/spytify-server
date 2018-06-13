48d78f4a31d3b3860aaab868cb635c8da48884d6
Frederico Martins <fredericomartins@outlook.com>
2018-06-13 18:22:32 +0100

    Library added and music parsing bug solved

--- Scripts/music-downloader.py
+++ Scripts/music-downloader.py
@@ -5,7 +5,7 @@
 
 from __future__ import unicode_literals
 
-from os import listdir, path
+from os import listdir, makedirs, path
 from shutil import move
 from sys import argv
 from youtube_dl import YoutubeDL
@@ -29,8 +29,25 @@ with YoutubeDL(ydl_opts) as ydl, open(argv[1], 'r') as openfile:
 
 for each in listdir('.'):
     if path.isfile(each) and each[-4:] == '.mp3':
-        print("Moving '{0}'")
-        artist = input("Artist: ")
-        song = input("Song: ")
+        print("Moving '{0}'".format(each))
+        try:
+            artist, song, *rest = each.split('-')
+        except ValueError:
+            artist = song = ''
+        else:
+            artist = artist.strip()
+            song = song.strip()
 
-        move(each, '/home/flippy/Music/{0}/Unknown Album/{1}'.format(artist, song))
+        artist_input = input("Artist [{0}]: ".format(artist))
+        song_input = input("Song [{0}]: ".format(song))
+
+        if artist_input:
+            artist = artist_input
+        if song_input:
+            song = song_input
+
+        if not path.exists('/home/flippy/Music/{0}'.format(artist)):
+            makedirs('/home/flippy/Music/{0}'.format(artist))
+        if not path.exists('/home/flippy/Music/{0}/Unknown Album'.format(artist)):
+            makedirs('/home/flippy/Music/{0}/Unknown Album'.format(artist))
+        move(each, '/home/flippy/Music/{0}/Unknown Album/{1}.mp3'.format(artist, song))

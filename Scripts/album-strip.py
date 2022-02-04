0d6a706bb643f506a02fab0c9a20f32080996ad2
Frederico Martins <22522942+fredericomateusmartins@users.noreply.github.com>
2022-02-04 16:40:39 +0000

    Update album-strip.py

--- Scripts/album-strip.py
+++ Scripts/album-strip.py
@@ -13,7 +13,7 @@ def Parse():
                         -------------------
                         Music download conv
                     '''), epilog=dedent('''\
-                        Check the git repository at https://github.com/flippym/spytify-server,
+                        Check the git repository at https://github.com/fredericomateusmartins/spytify-server,
                         for more information about usage, documentation and bug report.\
                     ''')
                 )
